70f56fdedf9b2082ddbcef1564085b9d8b5f7564
Beglov Sergey <beglov.sergey@yandex.ru>
2020-02-22 13:25:24 +0300

    lesson 4

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.titles(category_title)
+    category = Category.find_by(title: category_title)
+    Test.where(category_id: category.id).order(title: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def tests(level = 0)
+    Test.where(user_id: id, level: level)
+  end
 end
