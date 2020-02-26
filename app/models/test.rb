886f9ce7bdc6cf6dfff9964800e06f4269b91e66
Beglov Sergey <beglov.sergey@yandex.ru>
2020-02-26 18:27:24 +0300

    lesson 5

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :author, class_name: 'User'
   belongs_to :category
-  has_and_belongs_to_many :users
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
 
   def self.titles(category_title)
     Test.joins(:category).where('categories.title = ?', category_title).order(title: :desc).pluck(:title)
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create
+user1 = User.create!
+user2 = User.create!
 categories = Category.create!([{title: 'Frontend'}, {title: 'Backend'}, {title: 'DevOps'}])
-tests = Test.create!([
-  {title: 'HTML', level: 0, category_id: categories[0].id},
-  {title: 'Go', level: 1, category_id: categories[1].id},
-  {title: 'Ruby', level: 1, category_id: categories[1].id},
-  {title: 'Docker', level: 2, category_id: categories[2].id}
+tests = user1.tests.create!([
+  {title: 'HTML', level: 0, category_id: categories[0].id, author_id: user2.id},
+  {title: 'Go', level: 1, category_id: categories[1].id, author_id: user2.id},
+  {title: 'Ruby', level: 1, category_id: categories[1].id, author_id: user2.id},
+  {title: 'Docker', level: 2, category_id: categories[2].id, author_id: user2.id}
 ])
-user.tests = tests
 questions = Question.create!([
   {body: 'Тег ссылки', test_id: tests[0].id},
   {body: 'Тег параграфа', test_id: tests[0].id},
