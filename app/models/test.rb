349ee113b7a2af3233b39700585e25cc360961b7
Beglov Sergey <beglov.sergey@yandex.ru>
2020-02-27 15:45:35 +0300

    lesson 6

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,10 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :complex, -> { where(level: 5..Float::INFINITY) }
+
   def self.titles(category_title)
     Test.joins(:category).where('categories.title = ?', category_title).order(title: :desc).pluck(:title)
   end
