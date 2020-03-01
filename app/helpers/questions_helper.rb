cda1ee1c3c3e4a8b4cacc787d933ceb957b7e003
Beglov Sergey <beglov.sergey@yandex.ru>
2020-03-01 21:56:45 +0300

    lesson 8 - add question_form_title helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_form_title(question)
+    if action_name == 'new'
+      "Create New #{question.test.title} Question"
+    elsif action_name == 'edit'
+      "Edit #{question.test.title} Question"
+    end
+  end
 end
