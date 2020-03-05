599d02119b2ea45b26a9bd90946b2831a0b7e320
Beglov Sergey <beglov.sergey@yandex.ru>
2020-03-05 23:35:49 +0300

    lesson 10 - fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to cookies[:target_url]
+      redirect_to cookies[:target_url] || root_path
     else
       flash.now[:alert] = 'Verify your Email and Password'
       render :new
