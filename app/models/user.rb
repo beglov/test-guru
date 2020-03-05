f4cab857353e3ede80bff8b40decc9705d0fb9c4
Beglov Sergey <beglov.sergey@yandex.ru>
2020-03-05 19:21:49 +0300

    lesson 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
+  # include Auth
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :author_id
 
-  validates :email, presence: true
+  validates :email, presence: true,
+                    format: {with: /\w+@\w+\.\w+/},
+                    uniqueness: true
+
+  has_secure_password
 
   def user_tests(level = 0)
     tests.where(level: level)
