d5a21f6db65dafd7d92effdda1dce43cb294f653
Beglov Sergey <beglov.sergey@yandex.ru>
2020-03-10 23:04:48 +0300

    lesson 11 - При регистрации пользователя запрашивать его имя и фамилию и после входа, выводить флэш-сообщение "Привет, <имя пользователя>!"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
   def after_sign_in_path_for(resource)
+    flash[:notice] = "Hello, #{current_user.first_name}!"
     if resource.is_a?(Admin)
       admin_tests_path
     else
