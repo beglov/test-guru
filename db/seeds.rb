a2342efc5cfd12b837769e441faa75a4ad750ed1
Beglov Sergey <beglov.sergey@yandex.ru>
2020-04-02 12:18:43 +0300

    lesson 17 - edit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-admin = Admin.create!(email: 'admin@admin.ru', password: '1q2w3e4r', first_name: 'FirstName', last_name: 'LastName')
+admin = Admin.create!(email: 'beglov.sergey@yandex.ru', password: '1q2w3e4r', first_name: 'Sergey', last_name: 'Beglov')
 categories = Category.create!([{title: 'Frontend'}, {title: 'Backend'}, {title: 'DevOps'}])
 tests = admin.tests.create!([
   {title: 'HTML', level: 0, category_id: categories[0].id, author_id: admin.id},
@@ -15,14 +15,23 @@ tests = admin.tests.create!([
   {title: 'Docker', level: 2, category_id: categories[2].id, author_id: admin.id}
 ])
 questions = Question.create!([
-  {body: 'Тег ссылки', test_id: tests[0].id},
-  {body: 'Тег параграфа', test_id: tests[0].id},
+  {body: 'Укажите тег ссылки', test_id: tests[0].id},
+  {body: 'Укажите тег параграфа', test_id: tests[0].id},
   {body: 'Ruby интерпретируемый язык или компилируемый?', test_id: tests[2].id},
   {body: 'Самый популярный фреймворк на руби', test_id: tests[2].id}
 ])
 Answer.create!([
-  {body: '<a>', question_id: questions[0].id},
-  {body: '<p>', question_id: questions[1].id},
-  {body: 'интерпретируемый', question_id: questions[2].id},
-  {body: 'Rails', question_id: questions[3].id}
+  {body: '<z>', question_id: questions[0].id},
+  {body: '<t>', question_id: questions[0].id},
+  {body: '<pre>', question_id: questions[0].id},
+  {body: '<a>', question_id: questions[0].id, correct: true},
+  {body: '<p>', question_id: questions[1].id, correct: true},
+  {body: '<g>', question_id: questions[1].id},
+  {body: '<input>', question_id: questions[1].id},
+  {body: '<i>', question_id: questions[1].id},
+  {body: 'интерпретируемый', question_id: questions[2].id, correct: true},
+  {body: 'компилируемый', question_id: questions[2].id},
+  {body: 'Rails', question_id: questions[3].id, correct: true},
+  {body: 'Laravel', question_id: questions[3].id},
+  {body: 'Sinatra', question_id: questions[3].id}
 ])
