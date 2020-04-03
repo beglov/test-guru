af85f018e913f567d51852ec57d25c60d85dbef9
Beglov Sergey <beglov.sergey@yandex.ru>
2020-04-03 13:55:42 +0300

    lesson 17 - fixes

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,6 +8,7 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :complex, -> { where(level: 5..Float::INFINITY) }
+  scope :with_questions, -> { joins(:questions).group('tests.id') }
   scope :by_category_title, ->(category_title) { joins(:category).where('categories.title = ?', category_title) }
 
   validates :title, presence: true,
